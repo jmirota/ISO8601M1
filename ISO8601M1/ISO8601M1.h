23de7f447447c10e73e8ab799fc95d561dd3c5f7
Jakub Mirota <mirota.jakub@gmail.com>
2023-12-06 20:31:27 +0100

    Initial Commit

---
+++ ISO8601M1/ISO8601M1.h
@@ -0,0 +1,18 @@
+//
+//  ISO8601M1.h
+//  ISO8601M1
+//
+//  Created by Jakub Mirota on 06/12/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ISO8601M1.
+FOUNDATION_EXPORT double ISO8601M1VersionNumber;
+
+//! Project version string for ISO8601M1.
+FOUNDATION_EXPORT const unsigned char ISO8601M1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISO8601M1/PublicHeader.h>
+
+
---
+++ ISO8601M1Tests/ISO8601M1Tests.m
@@ -0,0 +1,36 @@
+//
+//  ISO8601M1Tests.m
+//  ISO8601M1Tests
+//
+//  Created by Jakub Mirota on 06/12/2023.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ISO8601M1Tests : XCTestCase
+
+@end
+
+@implementation ISO8601M1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
